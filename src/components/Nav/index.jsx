4dfc448f58f46c1a83c3decd697b9f1081456c84
Daniel Noyola <danielnv18@gmail.com>
2018-05-05 18:51:46 -0600

    commented toggle menu.

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -45,7 +45,7 @@ class Nav extends React.Component {
 			<nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
 				<div className="container">
 					<Link to="/" className="navbar-brand">{siteTitle}</Link>
-					<button onClick={this.toggleMenu} className={`navbar-toggler navbar-toggler-right ${navState.button}`} type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+					<button className={`navbar-toggler navbar-toggler-right ${navState.button}`} type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
 						Menu
 						<i className="fa fa-bars"></i>
 					</button>
